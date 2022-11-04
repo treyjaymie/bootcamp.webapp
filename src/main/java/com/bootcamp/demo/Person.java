619c16fcdc4c6daef32416859e590999f7bb9eff
treyjaymie <antonio.treyjaymie@gmail.com>
2022-11-04 14:49:59 +0800

    Added address to Person

--- src/main/java/com/bootcamp/demo/Person.java
+++ src/main/java/com/bootcamp/demo/Person.java
@@ -3,13 +3,13 @@ package com.bootcamp.demo;
 public class Person {
 	private String firstName;
 	private String lastName;
+	private String address;
 	
-	
-	
-	public Person(String firstName, String lastName) {
+	public Person(String firstName, String lastName, String address) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
+		this.address = address;
 	}
 	
 	public String getFirstName() {
