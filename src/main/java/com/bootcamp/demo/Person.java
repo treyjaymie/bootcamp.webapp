a6ba026af66173d0e92f9442e233349c5683e234
treyjaymie <antonio.treyjaymie@gmail.com>
2022-11-04 14:51:28 +0800

    Added age property to Person

--- src/main/java/com/bootcamp/demo/Person.java
+++ src/main/java/com/bootcamp/demo/Person.java
@@ -4,12 +4,14 @@ public class Person {
 	private String firstName;
 	private String lastName;
 	private String address;
+	private int age;
 	
-	public Person(String firstName, String lastName, String address) {
+	public Person(String firstName, String lastName, String address, int age) {
 		super();
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.address = address;
+		this.age = age;
 	}
 	
 	public String getFirstName() {
@@ -24,6 +26,24 @@ public class Person {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
 	
 	
 }
